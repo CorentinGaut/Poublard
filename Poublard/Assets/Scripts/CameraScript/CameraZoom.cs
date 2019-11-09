995655db047806637e2b6786ddb9ff158eb2bd61
amir khaled <livebox07@live.fr>
2019-11-09 20:07:36 +0100

    Camera script

---
+++ Poublard/Assets/Scripts/CameraScript/CameraZoom.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[RequireComponent(typeof(Camera))]
+public class CameraZoom : MonoBehaviour
+{
+    public List<Transform> targets;
+
+    public Vector3 offset;
+    public float smoothTime = .5f;
+    public float minZoom = 40f;
+    public float maxZoom = 10f;
+    public float zoomLimiter = 50f;
+
+    Vector3 velocity;
+    Camera cam;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    private void LateUpdate()
+    {
+        if (targets.Count == 0)
+            return;
+
+        Move();
+        Zoom();
+    }
+
+    private void Zoom()
+    {
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatesDistance() / zoomLimiter);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView,newZoom , Time.deltaTime);
+    }
+
+
+    private void Move()
+    {
+        Vector3 centerPoint = GetCenterPoint();
+        Vector3 newPosition = centerPoint + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+    }
+
+
+
+    private float GetGreatesDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+
+        }
+
+
+        return (bounds.size.x> bounds.size.z)? bounds.size.x : bounds.size.z;
+
+    }
+
+
+    private Vector3 GetCenterPoint()
+    {
+        if(targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.center;
+    }
+}
