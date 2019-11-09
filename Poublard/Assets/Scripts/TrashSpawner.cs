689b83658c7e9eae6d95a51c6eb57c9ccb8383ab
Homère <homerebourgeois@hotmail.fr>
2019-11-09 16:48:59 +0100

    ajout collider et rigidbody au detritus + spawn dechets

---
+++ Poublard/Assets/Scripts/TrashSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashSpawner : MonoBehaviour
+{
+    public List<GameObject> objects;
+
+    public int nbTrashSpawned;
+
+    public float minPosX;
+    public float minPosZ;
+    public float maxPosX;
+    public float maxPosZ;
+
+    public float ejectionDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnTrash();
+    }
+
+    private void SpawnTrash()
+    {
+        for(int i = 0;i<nbTrashSpawned;i++)
+        {
+            GameObject go = Instantiate(objects[UnityEngine.Random.Range(0, objects.Count)]);
+            go.transform.position = new Vector3(UnityEngine.Random.Range(minPosX, maxPosX), 0,UnityEngine.Random.Range(minPosZ, maxPosZ));
+            float angle = UnityEngine.Random.Range(-Mathf.PI, Mathf.PI);
+            go.GetComponent<Rigidbody>().AddForce(new Vector3(Mathf.Cos(angle)*ejectionDistance, ejectionDistance,  Mathf.Sin(angle)*ejectionDistance),ForceMode.Impulse);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
