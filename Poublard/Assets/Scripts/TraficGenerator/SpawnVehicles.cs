0d67ca655dfe7ecaf6293d91b988f4e57906eb37
Khadath <corentin.gautier@etu.udamail.fr>
2019-11-09 13:49:08 +0100

    Ajout du trafic generator + des prefabs de canettes

---
+++ Poublard/Assets/Scripts/TraficGenerator/SpawnVehicles.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnVehicles : MonoBehaviour
+{
+    public GameObject[] vehicles;
+    public float TimerSpawnMin = 2.0f;
+    public float TimerSpawnMax = 5.0f;
+
+    private float TimerRand;
+    private float Timer = 0.0f;
+    private GameObject randVehicles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimerRand = RandomTimerSpawn();
+        randVehicles = vehicles[0];
+        Instantiate(randVehicles, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        if (Timer >= TimerRand)
+        {
+            RandomVehicles();
+            Instantiate(randVehicles, transform.position, transform.rotation);
+            TimerRand = RandomTimerSpawn();
+            Timer = 0.0f;
+        }
+        
+    }
+
+    void RandomVehicles()
+    {
+        int rand = Random.Range(0, vehicles.Length);
+        randVehicles = vehicles[rand];
+    }
+    float RandomTimerSpawn()
+    {
+        return Random.Range(TimerSpawnMin, TimerSpawnMax);
+    }
+}
---
+++ Poublard/Assets/Scripts/TraficGenerator/VehiclesMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehiclesMovement : MonoBehaviour
+{
+    public float speed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += this.transform.forward * speed * Time.deltaTime;
+    }
+}
