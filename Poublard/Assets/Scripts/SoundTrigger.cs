4f63e161bff95c443da5de8580e8c110c0f64370
Léo <leoveyssiere.pro@hotmail.fr>
2019-11-09 19:19:56 +0100

    Intégration de la musique de splashScreen et Menu

---
+++ Poublard/Assets/Scripts/SoundTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTrigger : MonoBehaviour
+{
+    public AudioClip audioClip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.FindObjectOfType<GameManager>().GetComponent<AudioSource>().clip = audioClip;
+        GameObject.FindObjectOfType<GameManager>().GetComponent<AudioSource>().Play();
+    }
+}
