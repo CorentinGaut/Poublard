5f24bbde4623f39c62307d8a0caf1d4a45d98236
Léo <leoveyssiere.pro@hotmail.fr>
2019-11-09 19:55:31 +0100

    Améliorations sur les soundTrigger : permet de changer le Loop ou pas

--- Poublard/Assets/Scripts/SoundTrigger.cs
+++ Poublard/Assets/Scripts/SoundTrigger.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class SoundTrigger : MonoBehaviour
 {
     public AudioClip audioClip;
+    public bool setLoop;
     // Start is called before the first frame update
     void Start()
     {
         GameObject.FindObjectOfType<GameManager>().GetComponent<AudioSource>().clip = audioClip;
+        GameObject.FindObjectOfType<GameManager>().GetComponent<AudioSource>().loop = setLoop;
         GameObject.FindObjectOfType<GameManager>().GetComponent<AudioSource>().Play();
     }
 }
