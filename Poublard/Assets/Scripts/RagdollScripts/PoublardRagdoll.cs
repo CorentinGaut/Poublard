6ff2e435295742ee2803120be410b4391064a2e6
videti <dev.detienne@gmail.com>
2019-11-10 23:04:29 +0100

    - added respawn

--- Poublard/Assets/Scripts/RagdollScripts/PoublardRagdoll.cs
+++ Poublard/Assets/Scripts/RagdollScripts/PoublardRagdoll.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 public class PoublardRagdoll : MonoBehaviour
@@ -9,6 +11,24 @@ public class PoublardRagdoll : MonoBehaviour
     public int controllerNumber;
     public float jumpMultiplicator = 1f, punchMultiplicator = 1f, speedMultiplicator = 1f;
     public float punchForce = 150f, jumpForce = 1500f;
+    public bool dead = false, respawn = false;
+    Vector3[] defaultPosChildren;
+    public GameObject prefab;
+    Coroutine respawnCoroutine;
+
+    private void Awake()
+    {
+        if(prefab == null)
+        {
+            prefab = gameObject;
+        }
+        Transform[] tr_children = GetComponentsInChildren<Transform>();
+        defaultPosChildren = new Vector3[tr_children.Length];
+        for (int i = 0; i < tr_children.Length; i++)
+        {
+            defaultPosChildren[i] = tr_children[i].position;
+        }
+    }
 
     void ReinitJumpMultiplicator()
     {
@@ -40,5 +60,54 @@ public class PoublardRagdoll : MonoBehaviour
         jumpMultiplicator = newMultiplicator;
         CancelInvoke("ReinitSpeedMultiplicator");
         Invoke("ReinitSpeedMultiplicator", time);
+    }
+
+    private void Update()
+    {
+        if (dead)
+        {
+            Array.ForEach(GetComponentsInChildren<ConfigurableJoint>(), x => Destroy(x));
+            Destroy(transform.Find("Pelvis").GetComponent<AddPermanentForces>());
+            Destroy(GetComponentInChildren<FallDetection>());
+            //Destroy(this);
+        }
+        if (dead && respawn)
+        {
+            respawn = false;
+            dead = false;
+            respawnCoroutine = StartCoroutine(Respawn());
+            
+        }
+    }
+
+    IEnumerator Respawn()
+    {
+        Array.ForEach(GetComponentsInChildren<Collider>(), x => x.enabled = false);
+        float timer = 0;
+        float totalTime = 1f;
+        Transform[] tr_children = GetComponentsInChildren<Transform>();
+        Vector3[] finalPosChildren = new Vector3[tr_children.Length];
+        for (int i = 0; i < tr_children.Length; i++)
+        {
+            finalPosChildren[i] = tr_children[i].position;
+        }
+
+        while (timer < totalTime)
+        {
+            yield return null;
+            timer += Time.deltaTime;
+            
+            for (int i = 0; i < tr_children.Length; i++)
+            {
+                tr_children[i].position = Vector3.Lerp(finalPosChildren[i], defaultPosChildren[i], timer / totalTime);
+            }
+        }
+        GameObject newRagdoll = Instantiate(prefab, transform.position, transform.rotation, transform.parent);
+        newRagdoll.name = name;
+        newRagdoll.GetComponent<PoublardRagdoll>().controllerNumber = controllerNumber;
+        newRagdoll.GetComponent<PoublardRagdoll>().dead = false;
+        newRagdoll.GetComponent<PoublardRagdoll>().respawn = false;
+        Array.ForEach(newRagdoll.GetComponentsInChildren<Collider>(), x => x.enabled = true);
+        Destroy(gameObject);
     }
 }
