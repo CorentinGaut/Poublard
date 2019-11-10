7d2792a526990e4523513c547383679aa95d65fd
videti <dev.detienne@gmail.com>
2019-11-10 19:13:06 +0100

    - added catchableobject script

---
+++ Poublard/Assets/Scripts/RagdollScripts/CatchableObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchableObject : MonoBehaviour
+{
+    Rigidbody _rigidbody;
+    float defaultMass;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        defaultMass = _rigidbody.mass;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(GetComponent<ConfigurableJoint>() == null)
+        {
+            _rigidbody.mass = defaultMass;
+        } else
+        {
+            _rigidbody.mass = 0.01f;
+        }
+    }
+}
