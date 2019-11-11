e7ea44510d56ff8e7fa3d43e2d5e09ffe8543c38
Léo <leoveyssiere.pro@hotmail.fr>
2019-11-11 10:49:41 +0100

    Musique du jeu intégrée

---
+++ Poublard/Assets/SoundPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+    public AudioClip audioClip;
+    public float timeBeforeDestroy;
+    public bool loop;
+
+    private float count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<AudioSource>().loop = loop;
+        GetComponent<AudioSource>().clip = audioClip;
+        GetComponent<AudioSource>().Play();
+    }
+
+    private void Update()
+    {
+        count += Time.deltaTime;
+        if (count >= timeBeforeDestroy)
+        {
+            Destroy(gameObject);
+        }
+    }   
+}
