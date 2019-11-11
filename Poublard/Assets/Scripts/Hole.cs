dc43c70a7a89af39d439904aee68246fba56aa42
Corentin <corentin.chignin@hotmail.fr>
2019-11-11 15:10:19 +0100

    Auto stash before merge of "master" and "origin/master"

--- Poublard/Assets/Scripts/Hole.cs
+++ Poublard/Assets/Scripts/Hole.cs
@@ -8,6 +8,9 @@ public class Hole : MonoBehaviour
     public GameObject exit;
     public Vector3 ejectionVector;
     public float maxAngle;
+    public SoundPlayer soundPlayerPrefab;
+    public AudioClip effectSound;
+    public AudioClip effectSound2;
 
     private void Start()
     {
@@ -42,6 +45,11 @@ public class Hole : MonoBehaviour
         //{
         //    r.enabled = false;
         //}
+        SoundPlayer blub = Instantiate(soundPlayerPrefab, transform.position,Quaternion.identity);
+        blub.loop = false;
+        blub.timeBeforeDestroy=1.0f;
+        blub.audioClip = effectSound;
+        blub.volume = 0.6f;
 
         yield return new WaitForSeconds(1.0f);
 
@@ -59,11 +67,16 @@ public class Hole : MonoBehaviour
             if (c.GetComponent<Rigidbody>() != null)
                 c.GetComponent<Rigidbody>().velocity = (ejectionVector + new Vector3(Mathf.Cos(angle) * ejectionForce, ejectionForce, Mathf.Sin(angle) *ejectionForce));
         }
-
+        cc.GetComponent<AddPermanentForces>().yForce = 0;
         //foreach (Renderer r in rends)
         //{
         //    r.enabled = true;
         //}       
+        SoundPlayer blib = Instantiate(soundPlayerPrefab, transform.position, Quaternion.identity);
+        blib.loop = false;
+        blib.timeBeforeDestroy = 1.0f;
+        blib.audioClip = effectSound2;
+        blib.volume = 0.6f;
         yield return new WaitForSeconds(0.3f);
 
 
